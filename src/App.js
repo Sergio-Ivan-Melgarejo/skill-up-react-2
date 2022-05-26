6dde7ae65010abfe332cc2a1e0aba32422c410ef
Sergio Ivan Melgarejo <sergio.ivan.melgarejo@gmail.com>
2022-05-26 16:11:26 -0300

    Unit 2

--- src/App.js
+++ src/App.js
@@ -1,2 +1,18 @@
 import './App.css';
-export const App = () => <div>Go scrum</div>
+
+export default function App () {
+  return (
+    <div className='container'>
+      <form>
+        <h1>Iniciar sesión</h1>
+        <label htmlFor='email'>Email</label>
+        <input type="email" name="email" id="email" />
+        <label htmlFor='password'>Contraseña</label>
+        <input type="password" name="password" id="password" />
+        <div>
+          <button type="submit">Enviar</button>
+        </div>
+      </form>
+    </div>
+  )
+} 
