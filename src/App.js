570f82ed5b628b59d8458292ddb3945dd444e807
Sergio Ivan Melgarejo <88751144+Sergio-Ivan-Melgarejo@users.noreply.github.com>
2022-05-27 13:21:49 -0300

    unit 3 (#2)
    * practice form
    * added formik and login form
    * register form

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
+
+// Components
+import Login from "./components/Login/Login"
+// import Register from './components/Register/Register';
+
+// Style
 import './App.css';
 
 export default function App () {
   return (
     <div className='container'>
-      <form>
-        <h1>Iniciar sesión</h1>
-        <label htmlFor='email'>Email</label>
-        <input type="email" name="email" id="email" />
-        <label htmlFor='password'>Contraseña</label>
-        <input type="password" name="password" id="password" />
-        <div>
-          <button type="submit">Enviar</button>
-        </div>
-      </form>
+      <Login />
+      {/* <Register /> */}
     </div>
   )
 } 
