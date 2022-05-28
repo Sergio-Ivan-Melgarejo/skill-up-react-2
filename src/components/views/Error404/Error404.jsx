dfeb9fe2b4b8c577387057b3f0f5a3ae979c53e6
Sergio Ivan Melgarejo <88751144+Sergio-Ivan-Melgarejo@users.noreply.github.com>
2022-05-28 20:27:44 -0300

    unit 4 (#3)
    * Unit 2
    * practice form
    * added formik
    * register form
    * unit 3 (#2)
    * practice form
    * added formik and login form
    * register form
    * added react-router-dom/@6
    * unit 4

---
+++ src/components/views/Error404/Error404.jsx
@@ -0,0 +1,8 @@
+const Error404 = () => {
+    return <div className="container">
+        <h2>Error404</h2>
+        <p>no se encontro la página solicitada</p>
+    </div>;
+};
+
+export default Error404;
