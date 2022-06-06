43045d5aae46209405fe1b653c053aa05c577ba0
Sergio Ivan Melgarejo <sergio.ivan.melgarejo@gmail.com>
2022-06-06 10:57:08 -0300

    added reducers and store

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import './index.css';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import App from './App';
+import { store } from './store/store';
+import './index.css';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
-  </React.StrictMode>
+  </StrictMode>
 );
