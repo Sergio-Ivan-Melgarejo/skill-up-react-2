d288060f131afb5b54407f7c3035bf7c7a7a7c9c
Sergio Ivan Melgarejo <sergio.ivan.melgarejo@gmail.com>
2022-06-06 11:44:53 -0300

    added creates action and reducer

--- src/store/reducers/tasksReducer.js
+++ src/store/reducers/tasksReducer.js
@@ -1,9 +1,30 @@
+import { TASKS_SUCCESS, TASKS_REQUEST, TASKS_FAILURE } from "../types";
+
 const initialState = {
     loading: false,
     tasks: [],
     error:""
 }
 
-export const tasksReducer = (state = initialState) => {
-    return state
+export const tasksReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case TASKS_REQUEST:
+            return {
+                ...state,
+                loading: true
+            }
+        case TASKS_SUCCESS:
+            return {
+                ...state,
+                tasks: action.peyload
+            }
+        case TASKS_FAILURE:
+            return {
+                loading: false,
+                error: action.peyload,
+                tasks: []
+            }
+        default:
+            return state
+    }
 }
