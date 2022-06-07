e20f954d23eb4c4b8ed98037b17da2cb33233bd9
Sergio Ivan Melgarejo <sergio.ivan.melgarejo@gmail.com>
2022-06-07 11:10:14 -0300

    added tasks count

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
-import "./header.css";
-import img from "../../img/logo/logo_small.png";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+
+import img from "../../img/logo/logo_small.png";
+import "./header.css";
 
 const Header = () => {
   const navigate = useNavigate()
   
+  const { tasks } = useSelector( state => state.tasksReducer)
+
   const handleLogOut = () => {
     localStorage.removeItem("token");
     localStorage.removeItem("userName");
@@ -15,7 +19,8 @@ const Header = () => {
     <header>
       <img src={img} alt="Go Scrum" />
       <div className="wrapper_right_header">
-        <div>{localStorage.getItem("userName")}</div>
+        <div className="back">Tareas creadas: {tasks.length}</div>
+        <div className="back">{localStorage.getItem("userName")}</div>
         <div className="close" onClick={handleLogOut}>x</div>
       </div>
     </header>
