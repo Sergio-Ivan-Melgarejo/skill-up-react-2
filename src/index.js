84caf366e27f985c55068f040a2a6628a3e0c48b
Sergio Ivan Melgarejo <sergio.ivan.melgarejo@gmail.com>
2022-06-08 14:15:08 -0300

    added hashRouter and app title

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import App from './App';
@@ -10,10 +10,10 @@ import './index.css';
 const root = createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
-    <BrowserRouter basename='skill-up-react-2/'>
+    <HashRouter basename='skill-up-react-2/'>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
