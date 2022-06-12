ba6e8c07f617ce9dea17ca5319bba80b93ab2a7b
Sergio Ivan Melgarejo <sergio.ivan.melgarejo@gmail.com>
2022-06-11 23:44:53 -0300

    added footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,14 @@
+import { useState } from "react";
 import "./footer.css"
 
 const Footer = () => {
-  return <footer>
+  const [click, setClick] = useState(false);
+  return <footer className={click ? "open" : ""}>
+      <div onClick={()=>setClick(prev => !prev)} className="footer__button">
+        <div className="button__line"></div>
+        <div className="button__line"></div>
+        <div className="button__line"></div>
+      </div>
       ID de equipo: <span>{localStorage.getItem("teamID")}</span>
   </footer>;
 };
